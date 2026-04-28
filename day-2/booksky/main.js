ef96e4bfb8608b14ca14a79897b0b4f450302e93
Abinaya-251 <aabinayaantony251@gmail.com>
2026-04-28 13:06:10 +0530

    changes of day 2

--- day-2/booksky/main.js
+++ day-2/booksky/main.js
@@ -1,4 +1,39 @@
-var delbtn = document.querySelector(".delete-btn");
+
 function deleteBook(event) {
-    event.target.parentElement.remove();
+    console.log("delete book called")
+    event.target.closest(".book-card").remove();
 }
+
+var overlay = document.querySelector(".overlay");
+var popup = document.querySelector(".popup");
+var abtn = document.querySelector(".add-btn");
+abtn.addEventListener("click", function() {
+    overlay.style.display = "block";
+    popup.style.display = "block";
+})
+
+var cbtn = document.querySelector(".close-btn");
+cbtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    overlay.style.display = "none";
+    popup.style.display = "none";
+})
+var addbtn = document.querySelector(".submit-btn");
+addbtn.addEventListener("click", function() {
+    event.preventDefault();
+    overlay.style.display = "none";
+    popup.style.display = "none";
+    var bookTitle = document.getElementById("book-title").value;
+    var bookAuthor = document.getElementById("book-author").value;
+    var bookDescription = document.getElementById("book-description").value;
+    var divclass = document.querySelector(".container");
+    var divel = document.createElement("div");
+    divel.className = "book-card";
+    divel.innerHTML = `<h2 class="poppins">${bookTitle}</h2>
+            <h2>${bookAuthor}</h2>
+            <p class="poppins-thin">${bookDescription}</p>
+            <button class="delete-btn" onclick='deleteBook(event)'>Delete</button>`;
+    divclass.append(divel);
+    document.getElementById("book-form").reset();
+})
+
